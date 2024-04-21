6915e58c66b455c16812416dbca3a73078ae9eee
neerajdhurandher <neerajdhurandar@gmail.com>
2024-04-21 10:46:28 +0000

    GitHub action for commit : replace hard coded folder name with dynamic value

---
+++ precache-manifest.b1010620b8298a60ba1c79fd3265473e.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc4b2daf3215aeb7b4a4090b95be060",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9917beefca459d83ce55",
+    "url": "/static/css/2.14a57ef1.chunk.css"
+  },
+  {
+    "revision": "120c01d4499da3036348",
+    "url": "/static/css/main.e1b660f2.chunk.css"
+  },
+  {
+    "revision": "9917beefca459d83ce55",
+    "url": "/static/js/2.19f1436d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.19f1436d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120c01d4499da3036348",
+    "url": "/static/js/main.e1feda0b.chunk.js"
+  },
+  {
+    "revision": "b51b8325a5524a0946d9",
+    "url": "/static/js/runtime-main.29540a41.js"
+  },
+  {
+    "revision": "99e45ed3fc70cb48b0d929d04cd47f64",
+    "url": "/static/media/IndeDrum.99e45ed3.jpg"
+  },
+  {
+    "revision": "6d4bb5169127d01f0896d0442ae963e4",
+    "url": "/static/media/android_studio_logo.6d4bb516.png"
+  },
+  {
+    "revision": "e2e312083bc8806dd6f9be0b126802ce",
+    "url": "/static/media/arrow.e2e31208.svg"
+  },
+  {
+    "revision": "cda0452ec94513bc3e9a9a44a8ecf277",
+    "url": "/static/media/clgEventDrum.cda0452e.JPG"
+  },
+  {
+    "revision": "bffb371bda84cfa98146eea634998294",
+    "url": "/static/media/connections.bffb371b.png"
+  },
+  {
+    "revision": "295cd439fde21e03dd878f319df5f33e",
+    "url": "/static/media/contact-coding-bg.295cd439.jpg"
+  },
+  {
+    "revision": "82e4e0377578bd408458e364878dfe97",
+    "url": "/static/media/contact-info-icon1.82e4e037.svg"
+  },
+  {
+    "revision": "a56856319a8b25d4adc4e373b14cae7d",
+    "url": "/static/media/contact-info-icon2.a5685631.svg"
+  },
+  {
+    "revision": "19686534f09a814e76e340c55eac88b2",
+    "url": "/static/media/contact-info-icon3.19686534.svg"
+  },
+  {
+    "revision": "e1d5f380ca878cc2d97158306bfc7d52",
+    "url": "/static/media/django_logo.e1d5f380.png"
+  },
+  {
+    "revision": "503430894bcbebfbfcc8a4ce7ad64c3a",
+    "url": "/static/media/dsa_logo.50343089.png"
+  },
+  {
+    "revision": "169a173b74643691d88b0468f4b554d1",
+    "url": "/static/media/java_logo.169a173b.png"
+  },
+  {
+    "revision": "2496fd14978b169f9efb0689216e2af7",
+    "url": "/static/media/my_photo.2496fd14.png"
+  },
+  {
+    "revision": "07adc84dec920e954987329055c5acfe",
+    "url": "/static/media/my_website_thumbnail.07adc84d.png"
+  },
+  {
+    "revision": "930673df0f3630e7af11aa55b821ec77",
+    "url": "/static/media/neeraj_dhurandher_resume.930673df.pdf"
+  },
+  {
+    "revision": "faa35fc0d2aa9e6f860c1a1bb6653d6f",
+    "url": "/static/media/neeraj_logo_svg.faa35fc0.svg"
+  },
+  {
+    "revision": "b7e94e70c7e32913027260c88cfcd98c",
+    "url": "/static/media/rangDrum.b7e94e70.jpg"
+  },
+  {
+    "revision": "350ce1d103d8fdefb9be70ef127da6f8",
+    "url": "/static/media/reactjs_logo.350ce1d1.png"
+  },
+  {
+    "revision": "cd910bea095cca040ec0ff071c69de5d",
+    "url": "/static/media/repbulicDrum.cd910bea.JPG"
+  },
+  {
+    "revision": "fc486c96a5bde148803bbc825fd249e3",
+    "url": "/static/media/shakuntala_logo.fc486c96.png"
+  },
+  {
+    "revision": "eed868f9be2e37ff4a5ed02b344d1c79",
+    "url": "/static/media/spring.eed868f9.png"
+  },
+  {
+    "revision": "e13b7f17a4f7c424c3a68c878e0f3dc6",
+    "url": "/static/media/swachDrum.e13b7f17.png"
+  },
+  {
+    "revision": "13cb9c05a2a7bc0f894a1ede703cc067",
+    "url": "/static/media/vanmaDrum.13cb9c05.jpg"
+  },
+  {
+    "revision": "3bce0f979e4ef4d32c3008899b53c9b6",
+    "url": "/static/media/vizagDrum.3bce0f97.jpg"
+  },
+  {
+    "revision": "445b6f4536b73aac92a465caf64d6d67",
+    "url": "/static/media/vyomDrum.445b6f45.jpg"
+  },
+  {
+    "revision": "a47bc816b8da4a2f5b9d1c8a2c268155",
+    "url": "/static/media/yaari_logo.a47bc816.png"
+  }
+]);
